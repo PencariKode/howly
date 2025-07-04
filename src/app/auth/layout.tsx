d50efd996b67fa37438455b3f2096d7c22e4d42e
PencariKode <pencaricode@gmail.com>
2025-07-04 20:24:33 +0700

    add TypeScript compiler configuration and layout component

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,10 @@
+import {ReactNode, Suspense} from "react";
+
+
+export default function Layout({children}: { children: ReactNode }) {
+    return (
+        <Suspense>
+            {children}
+        </Suspense>
+    );
+};
